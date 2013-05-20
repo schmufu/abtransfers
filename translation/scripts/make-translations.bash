e06521c59d7364a2d9b5e06553f201a546ecfdf9
marko <marko@459a2fdc-afd4-4fb9-a8a9-e939c3f66bf0>
2013-05-20 13:09:58 +0000

    abt-mkae: make sure the right executables are called on MacOSX and Linux for translations
    git-svn-id: https://schmufu.dyndns.org/svn/ab_transfers/branch/abt-mkae@412 459a2fdc-afd4-4fb9-a8a9-e939c3f66bf0

--- translation/scripts/make-translations.bash
+++ translation/scripts/make-translations.bash
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+if [ `uname` = "Darwin" ]; then
+	PREFIX=/opt/macports-test
+else
+	PREFIX=/usr
+fi
+
+BIN=${PREFIX}/bin
 APP_RESOURCES=../abtransfers-build-Desktop_Version_4-Release/build/abtransfers.app/Contents/Resources/
 
 if [ "$1" == "up" ]
 then
 	echo "Updating translations..."
-	lupdate abtransfers.pro
+	${BIN}/lupdate \
+		-source-language de_DE \
+		-target-language en_GB \
+		-locations relative \
+			abtransfers.pro
+
+	mv abtransfers.en_GB.ts translation
 else
 	echo "Releasing translations into app package..."
-	lrelease translation/abtransfers.en_GB.ts
+	${BIN}/lrelease translation/abtransfers.en_GB.ts
 	cp translation/abtransfers.en_GB.qm ${APP_RESOURCES}/abtransfers.en_US
 	mv translation/abtransfers.en_GB.qm ${APP_RESOURCES}
 fi
